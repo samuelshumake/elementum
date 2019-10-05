e494266bb215c2788ee65daa7f545a1c1fe96670
Samuel Shumake <shumakesam@gmail.com>
2019-10-04 20:43:29 -0500

    Added spell frames to show which spell the player is currently using

--- src/scenes/s1r1.js
+++ src/scenes/s1r1.js
@@ -57,6 +57,12 @@ export default class s1r1 extends Phaser.Scene {
 		this.load.image('platform', './assets/sprites/ground2.png');
 		this.load.image('airwave', './assets/sprites/airwave.png');
 
+		/* ---------- LOADS SPRITES FOR SPELL FRAMES ---------- */
+		this.load.image('airFrame', './assets/sprites/airFrame.png');
+		this.load.image('bubbleFrame', './assets/sprites/bubbleFrame.png');
+		this.load.image('fireFrame', './assets/sprites/fireFrame.png');
+		this.load.image('earthFrame', './assets/sprites/earthFrame.png');
+
 
 		/* ---------- LOADS LEVEL TILEMAP ---------- */
 	    this.load.image("tiles", "./assets/map/tileset.png");
@@ -86,7 +92,17 @@ export default class s1r1 extends Phaser.Scene {
 
 		/* ---------- STAGE-ROOM DEBUGGER ---------- */
 		this.posDebug = this.add.text(this.cameras.main.width - 175, 0, '');
-		var srDebug = this.add.text(0, 0, 'Stage 1, Room 1');
+		// var srDebug = this.add.text(0, 0, 'Stage 1, Room 1');
+
+		/* ---------- CREATES SPELL FRAMES ---------- */
+		this.fireFrame = this.add.sprite(32, 32, 'fireFrame');
+		this.earthFrame = this.add.sprite(95, 32, 'earthFrame');
+		this.bubbleFrame = this.add.sprite(158, 32, 'bubbleFrame');
+		this.airFrame = this.add.sprite(221, 32, 'airFrame');
+		this.fireFrame.setScale(2);
+		this.earthFrame.setScale(2);
+		this.bubbleFrame.setScale(2);
+		this.airFrame.setScale(2);
 
 
 		/* ---------- CREATES MAP ---------- */
@@ -139,6 +155,34 @@ export default class s1r1 extends Phaser.Scene {
 
 		/* ---------- POSITION DEBUGGER ---------- */
 		this.posDebug.setText(`Position: ${this.player.x-17}, ${-1*(this.player.y-568).toFixed(0)}`);
+
+		/* ---------- SPELL FRAME CHECKER ---------- */
+		switch (this.player.currentSpell) {
+			case 'fire':
+				this.fireFrame.alpha = 1;
+				this.earthFrame.alpha = 0.2;
+				this.bubbleFrame.alpha = 0.2;
+				this.airFrame.alpha = 0.2;
+				break;
+			case 'earth':
+				this.fireFrame.alpha = 0.2;
+				this.earthFrame.alpha = 1;
+				this.bubbleFrame.alpha = 0.2;
+				this.airFrame.alpha = 0.2;
+				break;
+			case 'water':
+				this.fireFrame.alpha = 0.2;
+				this.earthFrame.alpha = 0.2;
+				this.bubbleFrame.alpha = 1;
+				this.airFrame.alpha = 0.2;
+				break;
+			case 'air':
+				this.fireFrame.alpha = 0.2;
+				this.earthFrame.alpha = 0.2;
+				this.bubbleFrame.alpha = 0.2;
+				this.airFrame.alpha = 1;
+				break;
+		}
 
 		/* ---------- MOVES PLAYER ---------- */
 		this.player.move(this);
