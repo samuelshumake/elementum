269acf62c972ede7177e9e1d779980f3685f6880
Edward Mbata <mbatae@utexas.edu>
2019-11-04 13:59:32 -0600

    changes

--- src/sprites/PressurePlate.js
+++ src/sprites/PressurePlate.js
@@ -48,7 +48,10 @@ export default class PressurePlate extends Phaser.GameObjects.Sprite {
 			this.play("plateUp", true);
 			this.tripped = false;
 			object.forEach(function(i) {
-				i.move(scene, i.options[0], i.options[1]);
+				i.move(scene, i.opti
+
+
+					ons[0], i.options[1]);
 				let newCamera = scene.cameras.add(858, 384, 400, 200).startFollow(i.options[2]).setZoom(i.options[3]).fadeIn(700);
 				newCamera.setBounds(0, 0, 800, 640);
 				let cameraFrame = scene.add.sprite(849, 398, 'cameraFrame').setScale(3.5, 3).setScrollFactor(0, 0);
