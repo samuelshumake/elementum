a456014b1047f2fa76912188303c7be10f3912bb
Samuel Shumake <shumakesam@gmail.com>
2019-11-08 12:58:56 -0600

    Fixed airwave bug that reversed direction of object when player turned

--- src/scenes/s0r4.js
+++ src/scenes/s0r4.js
@@ -216,14 +216,14 @@ export default class s0r4 extends Phaser.Scene {
 			if (this.rockGroup) {
 				for (let x in this.rockGroup) {
 					this.physics.add.overlap(this.rockGroup[x], this.player.airwave, () => {
-						this.player.airwave.push(this, this.rockGroup[x], this.player.flipX);
+						this.player.airwave.push(this, this.rockGroup[x], this.player.direction);
 					});
 				}
 			}
 			if (this.boxGroup) {
 				for (let x in this.boxGroup) {
 					this.physics.add.overlap(this.boxGroup[x], this.player.airwave, () => {
-						this.player.airwave.push(this, this.boxGroup[x], this.player.flipX);
+						this.player.airwave.push(this, this.boxGroup[x], this.player.direction);
 					});
 				}
 			}
