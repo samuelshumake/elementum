b2f181b6ce86863c0565cd92755bd732b9b82824
Jessica Lau <jessica_dlau@utexas.edu>
2019-11-18 11:11:17 -0600

    Created first air level s2r1

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import s1r2 from './scenes/s1r2.js';
 import s1r3 from './scenes/s1r3.js';
 import s1r4 from './scenes/s1r4.js';
 import s1r5 from './scenes/s1r5.js';
+import s2r1 from './scenes/s2r1.js';
 import Config from './config/config.js';
 
 class Game extends Phaser.Game {
@@ -30,7 +31,8 @@ class Game extends Phaser.Game {
 		this.scene.add('s1r3', s1r3);
 		this.scene.add('s1r4', s1r4);
 		this.scene.add('s1r5', s1r5);
-		this.scene.start('s1r1');
+		this.scene.add('s2r1', s2r1);
+		this.scene.start('s2r1');
   }
 }
 
